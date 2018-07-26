63324d52cf2b77ce8ef810575089e0a99e0ef94f
samsonjj <samsonjj@vcu.edu>
2018-07-26 15:20:56 -0400

    bacon

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import posts from './stubs/posts';
-if(localStorage.getItem('posts') === '') {
+if(localStorage.getItem('posts') === '' || localStorage.getItem('posts') === undefined) {
     localStorage.setItem('posts', JSON.stringify(posts));
 }
+
+if(localStorage.getItem('version') !== '') {
+    localStorage.setItem('posts', JSON.stringify(posts));
+    localStorage.setItem('version', '5');
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
